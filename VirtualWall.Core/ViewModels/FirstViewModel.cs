b53b2d462aa0945d2a252a0dc4a604f48d5fa397
Sean Grant <sean.grant@btinternet.com>
2014-06-06 09:58:38 +1000

    Added Filter

--- VirtualWall.Core/ViewModels/FirstViewModel.cs
+++ VirtualWall.Core/ViewModels/FirstViewModel.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Input;
 using Cirrious.MvvmCross.ViewModels;
 using VirtualWall.Core.Models.Trello;
@@ -9,6 +10,10 @@ namespace VirtualWall.Core.ViewModels
 {
     public class FirstViewModel : MvxViewModel
     {
+        private string _searchTerm;
+        private List<TrelloCard> _trelloCards;
+        private List<TrelloCard> _storyCards;
+
         public FirstViewModel(ICardService cardService, INfcService nfcService)
         {
             nfcService.DisplayCardAction = i =>
@@ -16,15 +21,47 @@ namespace VirtualWall.Core.ViewModels
                 var card = cardService.GetCardForId(i);
                 nfcService.DeepLinkTo(card.ShortUrl);
             };
-            
-            StoryCards = cardService.GetCards();
+
+            StoryCards = _trelloCards = cardService.GetCards();
         }
 
-        public List<TrelloCard> StoryCards { get; set; }
+        public string SearchTerm
+        {
+            get { return _searchTerm; }
+            set
+            {
+                _searchTerm = value; 
+                RaisePropertyChanged(() => SearchTerm);
+                Filter();
+            }
+        }
+
+        private void Filter()
+        {
+            StoryCards = _trelloCards.Where(x => x.Desc.Contains(SearchTerm) || x.Name.Contains(SearchTerm)).ToList();
+        }
+
+        public List<TrelloCard> StoryCards
+        {
+            get { return _storyCards; }
+            set { _storyCards = value; RaisePropertyChanged(() => StoryCards);}
+        }
 
         public ICommand ShowDetailCommand {
             get {
                 return new MvxCommand<TrelloCard>(item => ShowViewModel<CardDetailsViewModel>(new CardDetailsViewModel.Nav() { Id = item.ShortLink }));
+            }
+        }
+
+        private void ClearFilter()
+        {
+            SearchTerm = string.Empty;
+        }
+        public ICommand ClearFilterCommand
+        {
+            get
+            {
+                return new MvxCommand(ClearFilter);
             }
         }
     }
