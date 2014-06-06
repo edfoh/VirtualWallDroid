f07bf3bf5a813acf265b9db6395babb9a7895731
Edwin Foh <efoh@seek.com.au>
2014-06-06 13:11:27 +1000

    final

--- VirtualWall.Droid/Views/BoardsView.cs
+++ VirtualWall.Droid/Views/BoardsView.cs
@@ -9,17 +9,11 @@ namespace VirtualWall.Droid.Views
     [Activity(Label = "Boards")]
     public class BoardsView : MvxActivity
     {
-        private BindableProgress _bindableProgress;
 
         protected override void OnCreate(Bundle bundle) {
             base.OnCreate(bundle);
-            SetContentView(Resource.Layout.BoardsView);
+            SetContentView(Resource.Layout.BoardsView);            
 
-            _bindableProgress = new BindableProgress(this);
-
-            //var set = this.CreateBindingSet<BoardsView, BoardsViewModel>();
-            //set.Bind(_bindableProgress).For(p => p.Visible).To(vm => vm.IsBusy);
-            //set.Apply();
         }
     }
 }
